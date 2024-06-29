d6c560ee0790c8e2c4627fd63f5a046ac0dfb29e
syngha.han <gkstmdgk2731@naver.com>
2024-06-30 00:05:02 +0900

    chage root to src

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 // src/App.js
 
 import React from 'react';
-import MapComponent from './components/MapComponent';
+import MapComponent from 'components/MapComponent';
 
 const App = () => {
   const handleCountryClick = (countryName) => {
     alert(`Clicked on ${countryName}`);
-    // 클릭한 국가에 대한 추가적인 작업을 수행할 수 있습니다.
   };
 
   return (
--- src/components/Mapcomponent.jsx
+++ src/components/Mapcomponent.jsx
@@ -3,27 +3,35 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 
 const MapComponent = ({ onCountryClick }) => {
-  // 각 국가의 좌표와 팝업 메시지를 정의합니다.
   const countries = [
     { name: 'United States', position: [37.0902, -95.7129], message: 'Hello from USA!' },
     { name: 'South Korea', position: [35.9078, 127.7669], message: '안녕하세요 from 대한민국!' },
     // 필요한 다른 국가들을 추가할 수 있습니다.
   ];
 
+  // 지도의 최대 경계를 설정합니다.
+  const bounds = [
+    [-90, -180],
+    [90, 180]
+  ];
+
   return (
     <MapContainer
-      center={[0, 0]} // 초기 중심 위치
-      zoom={2} // 초기 줌 레벨
-      style={{ height: '100vh', width: '100%' }} // 맵의 스타일
+      center={[0, 0]}
+      zoom={2}
+      style={{ height: '100vh', width: '100%' }}
+      worldCopyJump={true}  // 지도 반복을 막습니다.
+      maxBounds={bounds}    // 지도 경계를 설정합니다.
+      maxBoundsViscosity={1.0}  // 경계를 넘으려는 시도를 방지합니다.
     >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       />
 
-      {/* 각 국가에 대한 마커와 팝업을 추가합니다. */}
       {countries.map((country, index) => (
         <Marker key={index} position={country.position} onClick={() => onCountryClick(country.name)}>
           <Popup>{country.message}</Popup>
